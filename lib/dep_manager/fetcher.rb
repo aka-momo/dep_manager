b33fdcc3b69be34a6d5b5d3b471025ed345e1115
Mohamed Diaa <mohamed.diaa27@gmail.com>
2016-12-26 15:59:53 +0200

    modifying fetcher module to render server error messages, adding local server url

--- lib/dep_manager/fetcher.rb
+++ lib/dep_manager/fetcher.rb
@@ -26,14 +26,19 @@ module DepManager
         res = Net::HTTP.start(@uri.hostname, @uri.port) do |http|
           http.request(req)
         end
-        return handle_server_response(res.body) if res.is_a? Net::HTTPSuccess
-      rescue => e
-        Logger.error e, 'Server Error'
+        parse_response(res)
+        return handle_server_response if res.is_a? Net::HTTPSuccess
+        handle_server_errors
       end
 
-      def handle_server_response(response_body)
-        parsed_response = JSON.parse(response_body)
-        parsed_response.each do |pack|
+      def parse_response(res)
+        @parsed_response = JSON.parse(res.body)
+      rescue
+        Logger.error StandardError, 'Server Not Reachable'
+      end
+
+      def handle_server_response
+        @parsed_response.each do |pack|
           @server_packages << pack['name']
           pack['system_dependencies'].each do |d|
             @server_dependencies << d['name']
@@ -43,11 +48,16 @@ module DepManager
         @server_dependencies
       end
 
-      def handle_missing_packahes
+      def handle_missing_packages
         missing_packages = @packages - @server_packages
         return if missing_packages.empty?
         msg = missing_packages.join(',') + 'are not listed in the server'
         Logger.warn(msg)
+      end
+
+      def handle_server_errors
+        msg = @parsed_response['error_message']
+        Logger.error(StandardError, msg) unless msg.nil?
       end
     end
   end
